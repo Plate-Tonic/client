6ebc53940e3b4acc40cbc00d23631297b5fc8f0f
montsieur <dndott@gmail.com>
2025-03-14 07:35:04 +1100

    Feature: Added random meal selection in home page and completed styling on web api

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/about.css";
 
 // Import images
-import about1 from "../assets/about-us-image.jpg"; // Ensure correct file extension
+import about1 from "../assets/about-us-image.jpg";
+import about2 from "../assets/02_about.png";
+import about3 from "../assets/03_about.png";
+import about4 from "../assets/04_about.png";
+import about5 from "../assets/05_about.png";
 
 // About Component
 const About = () => {
@@ -11,26 +15,38 @@ const About = () => {
       <div className="about-banner">What we are about</div>
 
       <div className="about-section">
-        <h2>Our Purpose</h2>
-        <p>We are dedicated to providing high-quality, nutritious meals to support a healthier lifestyle for everyone.</p>
-      </div>
-      
-      <div className="about-section">
-        <h2>Our Mission</h2>
-        <p>Our mission is to help you reach your health and fitness goals by providing you with the tools and resources you need to succeed.</p>
+        <img src={about2} className="about-image" alt="About Us" />
+        <div className="about-content">
+          <h2>Our Purpose</h2>
+          <p>We are dedicated to providing high-quality, nutritious meals to support a healthier lifestyle for everyone.</p>
+        </div>
       </div>
 
       <div className="about-section">
-        <h2>Our Vision</h2>
-        <p>Our vision is to create a healthier world by empowering people to make healthier choices.</p>
+        <img src={about3} className="about-image" alt="About Us" />
+        <div className="about-content">
+          <h2>Our Mission</h2>
+          <p>Our mission is to help you reach your health and fitness goals by providing you with the tools and resources you need to succeed.</p>
+        </div>
       </div>
 
       <div className="about-section">
-        <h2>Our Values</h2>
-        <p>Our core values include integrity, quality, and user satisfaction.</p>
+        <img src={about4} className="about-image" alt="About Us" />
+        <div className="about-content">
+          <h2>Our Vision</h2>
+          <p>Our vision is to create a healthier world by empowering people to make healthier choices.</p>
+        </div>
       </div>
 
-      <div className="contact-image">
+      <div className="about-section">
+        <img src={about5} className="about-image" alt="About Us" />
+        <div className="about-content">
+          <h2>Our Values</h2>
+          <p>Our core values include integrity, quality, and user satisfaction.</p>
+        </div>
+      </div>
+
+      <div className="about-bottom">
         <img src={about1} alt="About Us" />
       </div>
     </div>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../styles/homepage.css";
-import { FaUtensils, FaSmile, FaBoxOpen } from "react-icons/fa"; // Import icons
 
 // Import images locally
 import bannerImage from "../assets/01_banner.jpg";
+import icon1 from "../assets/01_homepage_icon.png";
+import icon2 from "../assets/02_homepage_icon.png";
+import icon3 from "../assets/03_homepage_icon.png";
+import icon4 from "../assets/04_homepage_icon.png";
 import testimonial1 from "../assets/istockphoto-636082286-612x612.jpg";
 import testimonial2 from "../assets/before-after-inline-2-cz-240705-4ec0d5.jpg";
 import testimonial3 from "../assets/katie-bolden-weight-loss-success-story-d90c538dd489423d94c6455dafd0db05.jpg";
-import marketstatistics from "../assets/Untitled-4.jpg";
-import meal1 from "../assets/marry-me-chicken2-65b3f9451efb6.avif";
-import meal2 from "../assets/lemon-shrimp-and-shaved-asparagus-66a174bf43c51.avif";
-import meal3 from "../assets/creole-shrimp-caesar-salad-with-cheesy-croutons-1677186680.avif";
 
 // Homepage Component
 const Homepage = () => {
+  
+  // State variables for random meal recommendations
+  const [randomMeals, setRandomMeals] = useState([]);
+
+  // Navigation hook for redirecting to other pages
+    const navigate = useNavigate();
+
+  // Fetch random meal recommendations
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_AUTH_API_URL}/meal-plan`);
+        const data = await response.json();
+
+        if (data && data.data && Array.isArray(data.data)) {
+          // Shuffle meals and select first three meals
+          const shuffled = data.data.sort(() => 0.5 - Math.random());
+          setRandomMeals(shuffled.slice(0, 3));
+        }
+      } catch (err) {
+        console.error("Error fetching meals:", err);
+      }
+    };
+
+    fetchMeals();
+  }, []);
 
   return (
     // Homepage Content
@@ -22,9 +48,13 @@ const Homepage = () => {
       {/* HERO BANNER */}
       <header className="homepage-header">
         <img src={bannerImage} alt="Hero Banner" className="banner-image" />
+
         <div className="banner-content">
-          <h1>Welcome to PLATE TONIC</h1>
-          <p>Delicious, nutritious meals tailored for you.</p>
+          <h3>Plate Tonic</h3>
+          <p>
+            "makes healthy eating effortless with personalized meal plans, delicious recipes, and smart tracking—helping you reach your goals without the guesswork."
+          </p>
+
           {/* Link to GetStarted Page */}
           <Link to="/getstarted" data-testid="get-started-button">
             <button type="button" className="get-started-button">
@@ -34,57 +64,72 @@ const Homepage = () => {
         </div>
       </header>
 
-      {/* MARKET STATISTICS */}
+      {/* MARKET SECTION */}
       <section className="homepage-market">
-        <img src={marketstatistics} alt="Market statistics graph" className="market-image" />
+        <h2>Eating smart has never been easier</h2>
+        <div className="market-grid">
+
+          <div className="market-item">
+            <img src={icon1} alt="eating" className="market-icon" />
+            <div>
+              <h3>Follow any eating style or create your own</h3>
+              <p>You can tailor your meals to fit popular diets like vegan and keto, aligning with your unique preferences and goals.</p>
+            </div>
+          </div>
+
+          <div className="market-item">
+            <img src={icon2} alt="reduce food waste" className="market-icon" />
+            <div>
+              <h3>Reduce food waste</h3>
+              <p>Minimize food waste by planning ahead. Our system suggests recipes that incorporate ingredients you may already have, ensuring nothing goes to waste.</p>
+            </div>
+          </div>
+
+          <div className="market-item">
+            <img src={icon3} alt="meal planning" className="market-icon" />
+            <div>
+              <h3>Take the anxiety out of picking what to eat</h3>
+              <p>Plan your meals in advance on your own terms, so when it’s time to eat, the decision is already made.
+              </p>
+            </div>
+          </div>
+
+          <div className="market-item">
+            <img src={icon4} alt="grocery" className="market-icon" />
+            <div>
+              <h3>Grocery shopping made easier</h3>
+              <p>Never miss a meal due to missing ingredients with our recipes that include all the ingredients you need.</p>
+            </div>
+          </div>
+
+        </div>
       </section>
 
-      {/* MARKET STATS SECTION */}
-      <section className="homepage-stats">
-
-        {/* <div className="stats-item">
-          <FaUtensils className="stats-icon" />
-          <h3>Updating Recipes</h3>
-          <p>Has never been easier</p>
-        </div>
-
-        <div className="stats-item">
-          <FaSmile className="stats-icon" />
-          <h3>100K+</h3>
-          <p>Satisfied Users</p>
-        </div>
-
-        <div className="stats-item">
-          <FaBoxOpen className="stats-icon" />
-          <h3>Deliveries</h3>
-          <p>COMING SOON</p>
-        </div> */}
-
-      </section>
 
       {/* MEAL RECOMMENDATIONS */}
       <section className="homepage-meals">
         <h2>Meal Recommendations</h2>
+        <p>"Discover delicious and nutritious meal options"</p>
 
         <div className="meal-container">
-
-          <div className="meal-item">
-            <img src={meal1} alt="Meal Option 1" className="meal-image" />
-            <p>Grilled Chicken with Brown Rice</p>
-            <strong>500 Calories</strong>
-          </div>
-
-          <div className="meal-item">
-            <img src={meal2} alt="Meal Option 2" className="meal-image" />
-            <p>Salmon & Roasted Vegetables</p>
-            <strong>450 Calories</strong>
-          </div>
-
-          <div className="meal-item">
-            <img src={meal3} alt="Meal Option 3" className="meal-image" />
-            <p>Quinoa & Chickpea Bowl</p>
-            <strong>400 Calories</strong>
-          </div>
+          {randomMeals.length > 0 ? (
+            randomMeals.map((meal) => (
+              <div key={meal._id} className="homepage-meal-item">
+                <img className="meal-image"
+                  src={`${import.meta.env.VITE_AUTH_API_URL}${meal.mealImage || "/uploads/placeholder-image.jpg"}`}
+                  alt={meal.name}
+                  onClick={() => navigate(`/meal/${meal._id}`, { state: { meal } })}
+                  crossOrigin="anonymous"
+                />
+                <p className="meal-name"
+                  onClick={() => navigate(`/meal/${meal._id}`, { state: { meal } })}
+                > {meal.name}
+                </p>
+              </div>
+            ))
+          ) : (
+            <p>Loading meals...</p>
+          )}
         </div>
 
       </section>
@@ -97,7 +142,7 @@ const Homepage = () => {
       {/* TESTIMONIALS SECTION */}
       <section className="homepage-testimonials">
         <h2>The Journey</h2>
-
+        <p>"Hear what our users are saying"</p>
         <div className="testimonial-container">
           <div className="testimonial-item">
             <img src={testimonial1} alt="Happy customer 1" className="testimonial-image" />
