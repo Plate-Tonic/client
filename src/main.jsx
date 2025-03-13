96cc0551b60b3f7dd0cee2b45d3f98c950372934
montsieur <dndott@gmail.com>
2025-03-13 19:31:08 +1100

    placed index.css in styles folder

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Import global styles
+import './styles/index.css'; // Import global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
